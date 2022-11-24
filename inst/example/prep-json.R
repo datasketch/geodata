4fc21f7ca64585525f9d7c22d86190590bf1d319
CamilaAchuri <camilaachury.4@gmail.com>
2022-11-24 17:07:09 -0300

    add Colombia with the largest San Andres

--- inst/example/prep-json.R
+++ inst/example/prep-json.R
@@ -1,7 +1,42 @@
+library(geodata)
+# prep colombia con su isla mas grande
+geodata:::clean_json(geoName = "colombia",
+                     geoFile = "inst/example/",
+                     geoId = "DPTO",
+                     geoProperties = "DPTO_NOMBRE",
+                     newnamesProperties = "name",
+                     jsonName = "col-departments", saveFile = TRUE, savePath = "inst/geodata/col/")
+
+
+# save this file in rds format
+tj <- read_lines("inst/geodata/col/col-departments.topojson")
+saveRDS(tj, "inst/geodata/col/col-departments.rds")
+
+# manually add the info of the topojson in the meta folder in the corresponding file
+# for eg, for this case the info add in col.yaml
+# then, restart session and use devtools to install changes
+devtools::load_all()
+devtools::document()
+devtools::install()
+"col_larg" %in% geodata::availableGeodata()
+
+# add centroids file
+geodata:::centroids_json(map_name = "col_larg", write = TRUE)
+geodata:::centroids_json(map_name = "col_larg", write = TRUE, ext = "rds")
+# Again restar and install pkg
+devtools::load_all()
+devtools::document()
+devtools::install()
+
+# now you can plot your map
+lfltmagic::lflt_choropleth_Gnm(map_name = "col_larg")
+
+
+
 
 # read and clean json to convert in topojson
 
-library(geodata)
+
 geodata:::clean_json(geoName = "alcaldias",
                      geoFile = "inst/example/",
                      geoId = "id",
